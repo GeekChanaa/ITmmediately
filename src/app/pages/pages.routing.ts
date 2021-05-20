fc8a111fedc10ee2d26698b648cfa8c405ff796b
GeekChanaa <chanaa.projects@gmail.com>
2021-05-20 13:41:09 +0100

    still working on responsive design

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutusComponent } from './Aboutus/Aboutus.component';
+import { ContactComponent } from './Contact/Contact.component';
 import { MainComponent } from './Main/Main.component';
 import { PagesComponent } from './pages.component';
 
@@ -13,7 +14,11 @@ export const appRoutes: Routes = [
   {
     path :'about-us',
     component : AboutusComponent,
-  }
+  },
+  {
+    path :'contact-us',
+    component : ContactComponent,
+  },
 ];
 
 @NgModule({
